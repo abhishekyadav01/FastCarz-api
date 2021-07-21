a253ad9a5ed74fd249c7422e90d4b4cdf16d0a92
Abhishek Yadav <abhishekyadav01@gmail.com>
2021-07-21 17:06:45 +0100

    Added variables and destroy stage in pipeline

--- azure/variables.tf
+++ azure/variables.tf
@@ -15,3 +15,20 @@ variable "subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
+
+variable "tag_business_unit" {
+  description = "Business Unit tagging"
+  default     = "macrolife"
+}
+
+variable "tag_cost_centre" {
+  description = "Cost Center tagging"
+  default     = "SOW-8739494"
+}
+
+
+variable "tag_environment" {
+  description = "Env type like dev,qa,prod"
+  type        = string
+  default     = "dev"
+}
